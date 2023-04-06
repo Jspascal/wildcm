6082bfe6ef3b1c0564994f37eea66692469c410a
Jspascal <fulgaros@gmail.com>
2023-04-06 23:44:51 +0100

    finished jwt integration on authservice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -63,7 +63,7 @@ export class AuthService {
 
     const secret = this.config.get('JWT_SECRET');
 
-    const token = this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret: secret,
     });
