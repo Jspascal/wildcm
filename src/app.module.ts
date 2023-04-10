104565f0cdcc5ebb36dd54f061a91b17ad772290
Jspascal <fulgaros@gmail.com>
2023-04-10 23:31:24 +0100

    making a custom guard for jwt, some modifications on jwt strategy and auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { BookmarkModule } from "./bookmark/bookmark.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
@@ -16,4 +16,4 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
